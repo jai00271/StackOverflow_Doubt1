5296c6b8a65860d1c415a887394b147f664ccacb
Mritunjai Pratap Singh <Mritunjai_PratapSing@Dell.com>
2019-04-16 08:24:23 +0530

    Add project files.

---
+++ Basics.Main/BaseConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Basic;
+using System;
+using System.Threading.Tasks;
+
+namespace Basics.Main
+{
+    public class BaseConsumer
+    {
+        public IDataRepository DataRepository = new DataRepository();
+
+        public async Task<string> AppSetting()
+        {
+            return await DataRepository.GetAppSettingAsync("name");
+        }
+    }
+}
---
+++ Basics.Main/DataRepository.cs
@@ -0,0 +1,22 @@
+﻿using Basic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.Main
+{
+    public class DataRepository: IDataRepository
+    {
+        private IDataRepository _repo;
+        public DataRepository()
+        {
+            _repo = new DataRepository();
+        }
+
+        public Task<string> GetAppSettingAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
